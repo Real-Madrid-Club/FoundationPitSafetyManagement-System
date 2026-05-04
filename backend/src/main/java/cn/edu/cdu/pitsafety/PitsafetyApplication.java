38757e28b5b95aff5d12bedaeccfa243119dc895
SoulSoulGoodman <2721167597@qq.com>
2026-05-04 22:54:13 +0800

    feat: 初始化 Spring Boot 后端基础骨架与核心依赖

---
+++ backend/src/main/java/cn/edu/cdu/pitsafety/PitsafetyApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.cdu.pitsafety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitsafetyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitsafetyApplication.class, args);
+    }
+
+}
